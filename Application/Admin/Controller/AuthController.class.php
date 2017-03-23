8ddde42d6a47a1a223aed41f501696e592277ee3
kevin.liu <791845283@qq.com>
2017-03-23 23:05:52 +0800

    处理菜单节点

--- Application/Admin/Controller/AuthController.class.php
+++ Application/Admin/Controller/AuthController.class.php
@@ -10,9 +10,16 @@ class AuthController extends PrivateController
     public function navlists()
     {
         $parentCate = M("auth_cate")->where("pid=0")->order('sort desc')->select();
-        foreach($parentCate as $cate){
-
+        foreach($parentCate as &$cate){
+            $son_cate = M("auth_cate")->where("pid={$cate['id']}")->order("sort desc")->select();
+            $cate['son_cate'] = $son_cate;
         }
+        $this->assign('parentCate',$parentCate);
+        $this->display();
+    }
+
+    public function addMenu()
+    {
         $this->display();
     }
 }
