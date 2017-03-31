2949ad4390d6ce26d7904ff719af2115a7e1a575
kevin.liu <791845283@qq.com>
2017-04-01 00:28:47 +0800

    composer

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -7,10 +7,16 @@
  */
 namespace Home\Controller;
 use Think\Controller;
-
+use QL\QueryList;
 class IndexController extends Controller{
 
     public function index(){
+
+
+        $hj = QueryList::Query('http://dayblog.cn',array('tt'=>array('h2 a','text'),'ll'=>array('h2 a','href')));
+//输出结果：二维关联数组
+        ppd($hj->data);
+
         ppd("welcome about you");
     }
 }
